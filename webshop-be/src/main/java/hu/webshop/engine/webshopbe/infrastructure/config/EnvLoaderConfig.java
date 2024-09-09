8cccff5dbe6f80800b4985117a86b8474fd8ef04
Kovács Domonkos <domonkos.kovacs@nitrowise.com>
2024-09-09 18:02:38 +0200

    add private noarg to envloader

--- webshop-be/src/main/java/hu/webshop/engine/webshopbe/infrastructure/config/EnvLoaderConfig.java
+++ webshop-be/src/main/java/hu/webshop/engine/webshopbe/infrastructure/config/EnvLoaderConfig.java
@@ -5,8 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
 @Configuration
 @PropertySource("file:${user.dir}/.env")
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class EnvLoaderConfig {
 
     @Bean
