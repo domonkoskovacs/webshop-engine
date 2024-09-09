e3ff06eb5e12e735e4c195f0fdec60fefa23d18a
Kovács Domonkos <domonkos.kovacs@nitrowise.com>
2024-09-09 16:40:02 +0200

    initial be project

---
+++ webshop-be/src/test/java/hu/webshop/engine/webshopbe/WebshopBeApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.webshop.engine.webshopbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
