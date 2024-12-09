6ab2880dc78068b0f43fa50662b3abda5f60f3cd
Kovács Domonkos <domonkos.kovacs@nitrowise.com>
2024-12-09 18:02:47 +0100

    work on storefront layout

---
+++ webshop-fe/src/components/storefront/Header.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "../ui/Select";
+
+const Header: React.FC = () => {
+    return (
+        <header className="flex items-center justify-between bg-muted py-1 px-4 text-xs">
+            <div>
+                <Link to="/faq" className="text-primary hover:underline">
+                    FAQ
+                </Link>
+            </div>
+
+            <div className="flex space-x-2">
+                <Select>
+                    <SelectTrigger className="h-6 border-none bg-muted">
+                        <SelectValue placeholder="USD"/>
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectItem value="USD">USD</SelectItem>
+                    </SelectContent>
+                </Select>
+
+                <Select>
+                    <SelectTrigger className="h-6 border-none bg-muted">
+                        <SelectValue placeholder="EN"/>
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectItem value="USD">EN</SelectItem>
+                    </SelectContent>
+                </Select>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
--- webshop-fe/src/components/storefront/Storefront.layout.tsx
+++ webshop-fe/src/components/storefront/Storefront.layout.tsx
@@ -3,6 +3,7 @@ import HomeBar from "./homebar/HomeBar.componenet";
 import Footer from "./Footer.component";
 import FooterDetails from "./FooterDetails.component";
 import {Separator} from "../ui/Separator";
+import Header from "./Header";
 
 interface LayoutProps {
     children: ReactNode;
@@ -12,7 +13,9 @@ const StorefrontLayout: React.FC<LayoutProps> = ({children}) => {
     return (
         <div className="flex flex-col min-h-screen scrollbar">
             <header>
+                <Header/>
                 <HomeBar/>
+                <Separator/>
             </header>
 
             <main className="flex-1 px-4 py-2">
