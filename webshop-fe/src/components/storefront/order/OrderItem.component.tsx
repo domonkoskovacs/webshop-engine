dc2b8fb3b9e206e53c8fa3bf928428987a474556
Kovács Domonkos <domonkos.kovacs@nitrowise.com>
2025-03-18 10:24:55 +0100

    update order item

--- webshop-fe/src/components/storefront/order/OrderItem.component.tsx
+++ webshop-fe/src/components/storefront/order/OrderItem.component.tsx
@@ -1,67 +1,93 @@
 import React, {useState} from "react";
-import {OrderResponse} from "../../../shared/api";
+import {OrderResponse, OrderResponseStatusEnum} from "../../../shared/api";
 import {Card, CardContent} from "../../ui/Card";
 import {Button} from "../../ui/Button";
-import {formatDate, isCancelable, isReturnable} from "../../../lib/order.utils";
+import {formatDate, getOrderActions} from "../../../lib/order.utils";
 import {Separator} from "../../ui/Separator";
+import {Link} from "react-router-dom";
+import {generateProductUrl} from "../../../lib/url.utils";
+import {useGender} from "../../../hooks/useGender";
 
 interface OrderItemProps {
     order: OrderResponse;
 }
 
 const OrderItem: React.FC<OrderItemProps> = ({order}) => {
+    const {gender} = useGender()
     const [showDetails, setShowDetails] = useState(false);
     const items = order.items || [];
-    const firstProductImageUrl = items[0]?.thumbNailUrl;
-    const secondProductImageUrl = items[1]?.thumbNailUrl;
+    const firstItem = items[0];
+    const secondItem = items[1];
+    const actions = getOrderActions(order);
+
+    const getProductGender = (itemGender?: string): string => {
+        if (!itemGender) return "";
+        return itemGender.toLowerCase() === "unisex"
+            ? gender.toLowerCase()
+            : itemGender.toLowerCase();
+    };
 
     return (
-        <Card key={order.id} className="w-full py-0 my-0">
+        <Card className="w-full py-0 my-0">
             <CardContent className="flex flex-col justify-center items-center sm:flex-row sm:justify-between">
                 <div className="flex flex-col sm:flex-row">
                     <div
-                        className={`flex ${items.length === 1 ? "justify-center" : "justify-start"} w-44 h-24 items-center gap-3`}
-                    >
-                        {firstProductImageUrl && (
-                            <img
-                                src={firstProductImageUrl}
-                                alt={items[0].productName || "No product name available"}
-                                className="w-20 h-20 object-cover rounded-md"
-                            />
+                        className={`flex ${items.length === 1 ? "justify-center" : "justify-start"} w-44 h-24 items-center gap-3`}>
+                        {firstItem?.thumbNailUrl && (
+                            <Link
+                                to={generateProductUrl(getProductGender(firstItem.gender), firstItem.categoryName, firstItem.subcategoryName, firstItem.productName, firstItem.productId)}>
+                                <img
+                                    src={firstItem.thumbNailUrl}
+                                    alt={firstItem.productName || "No product name available"}
+                                    className="w-20 h-20 object-cover rounded-md hover:opacity-80 transition"
+                                />
+                            </Link>
                         )}
-                        {items.length > 1 && secondProductImageUrl && (
-                            <img
-                                src={secondProductImageUrl}
-                                alt={items[1].productName || "No product name available"}
-                                className="w-20 h-20 object-cover rounded-md"
-                            />
+                        {items.length > 1 && secondItem?.thumbNailUrl && (
+                            <Link
+                                to={generateProductUrl(getProductGender(secondItem.gender), secondItem.categoryName, secondItem.subcategoryName, secondItem.productName, secondItem.productId)}>
+                                <img
+                                    src={secondItem.thumbNailUrl}
+                                    alt={secondItem.productName || "No product name available"}
+                                    className="w-20 h-20 object-cover rounded-md hover:opacity-80 transition"
+                                />
+                            </Link>
                         )}
                     </div>
-                    <div className="sm:ml-4 flex flex-col justify-center">
-                        <p>Status: <strong>{order.status}</strong></p>
-                        <p>Order date: <strong>{formatDate(order.orderDate)}</strong></p>
-                        <p className="text-lg font-semibold">Total: ${order.totalPrice!.toFixed(2)}</p>
+                    <div className="sm:ml-4 flex flex-col justify-center mt-4 sm:mt-0">
+                        <p>
+                            Status: <strong>{order.status}</strong>
+                        </p>
+                        <p>
+                            Order date: <strong>{formatDate(order.orderDate)}</strong>
+                        </p>
+                        <p className="text-lg font-semibold">
+                            Total: ${order.totalPrice?.toFixed(2)}
+                        </p>
                     </div>
                 </div>
-                <div className="text-left">
+                <div className="text-left mt-4 sm:mt-0">
                     <h3 className="text-lg font-semibold">Contact info:</h3>
-                    <p className="text-sm">Address: <strong>
-                        {order.address?.street ?? "N/A"}
-                        {order.address?.streetNumber ?? ""},
-                        {order.address?.city ?? "N/A"},
-                        {order.address?.zipCode ?? "N/A"},
-                        {order.address?.country ?? "N/A"}
-                    </strong></p>
-                    <p className="text-sm">Email: <strong>{order.email}</strong></p>
-                    <p className="text-sm">Phone: <strong>{order.phoneNumber}</strong></p>
+                    <p className="text-sm">
+                        Address: <strong>
+                        {order.address?.street ?? "N/A"} {order.address?.streetNumber ?? ""}, {order.address?.city ?? "N/A"}, {order.address?.zipCode ?? "N/A"}, {order.address?.country ?? "N/A"}
+                    </strong>
+                    </p>
+                    <p className="text-sm">
+                        Email: <strong>{order.email}</strong>
+                    </p>
+                    <p className="text-sm">
+                        Phone: <strong>{order.phoneNumber}</strong>
+                    </p>
                 </div>
-                <div className="flex flex-col items-end gap-2">
-                    {isCancelable(order) && <Button>
-                        Cancel Order
-                    </Button>}
-                    {isReturnable(order) && <Button>
-                        Return Items
-                    </Button>}
+                <div className="flex flex-col items-end gap-2 mt-4 sm:mt-0">
+                    {order.status === OrderResponseStatusEnum.Created && (
+                        <>
+                            {actions.canPay && <Button className="w-full">Pay Order</Button>}
+                            {actions.canCancel && <Button>Cancel Order</Button>}
+                        </>
+                    )}
+                    {actions.canReturn && <Button>Return Items</Button>}
                     <Button variant="link" onClick={() => setShowDetails(!showDetails)}>
                         {showDetails ? "Hide Details" : "Show Details"}
                     </Button>
@@ -71,17 +97,22 @@ const OrderItem: React.FC<OrderItemProps> = ({order}) => {
                 <div className="px-4 pb-4 border-t pt-2">
                     {items.map((orderItem, index) => {
                         const count = orderItem.count || 1;
-                        const productPrice = orderItem?.individualPrice || 0;
+                        const productPrice = orderItem.individualPrice || 0;
                         const totalProductPrice = productPrice * count;
                         return (
                             <div key={index} className="flex flex-row items-center gap-3 py-2">
-                                {orderItem && (
-                                    <img
-                                        src={orderItem.thumbNailUrl}
-                                        alt={orderItem.productName || "No product name available"}
-                                        className="w-16 h-16 object-cover rounded-md"
-                                    />
-                                )}
+                                <div className="w-16 h-16">
+                                    {orderItem.thumbNailUrl && (
+                                        <Link
+                                            to={generateProductUrl(getProductGender(orderItem.gender), orderItem.categoryName, orderItem.subcategoryName, orderItem.productName, orderItem.productId)}>
+                                            <img
+                                                src={orderItem.thumbNailUrl}
+                                                alt={orderItem.productName || "No product name available"}
+                                                className="w-16 h-16 object-cover rounded-md hover:opacity-80 transition"
+                                            />
+                                        </Link>
+                                    )}
+                                </div>
                                 <div>
                                     <p className="font-semibold">
                                         {orderItem.productName} (x{count})
@@ -94,9 +125,11 @@ const OrderItem: React.FC<OrderItemProps> = ({order}) => {
                         );
                     })}
                     <Separator/>
-                    <div className="mt-4">
+                    <div className="mt-4 text-sm">
                         <p>Order number: {order.orderNumber}</p>
-                        <p className="text-sm">Shipping Cost: ${(order.shippingPrice ?? NaN).toFixed(2)}</p>
+                        <p className="text-sm">
+                            Shipping Cost: ${order.shippingPrice?.toFixed(2)}
+                        </p>
                     </div>
                 </div>
             )}
--- webshop-fe/src/lib/order.utils.ts
+++ webshop-fe/src/lib/order.utils.ts
@@ -1,15 +1,46 @@
-import {OrderResponse} from "../shared/api";
+import { OrderResponse } from "../shared/api";
+import { OrderResponseStatusEnum } from "../shared/api";
 
-export function formatDate(date: string | undefined) {
+/**
+ * Formats a date string for display.
+ */
+export function formatDate(date: string | undefined): string {
     if (!date) return "-";
     const newDate = new Date(date);
     return newDate.toLocaleDateString();
 }
 
-export function isCancelable(order: OrderResponse) {
-    return true
+/**
+ * Returns true if the order can be paid.
+ * (Only orders in the CREATED state are considered payable.)
+ */
+export function isPayable(order: OrderResponse): boolean {
+    return order.status === OrderResponseStatusEnum.Created;
 }
 
-export function isReturnable(order: OrderResponse) {
-    return true
+/**
+ * Returns true if the order can be cancelled.
+ * (Only orders in the CREATED state are considered cancellable.)
+ */
+export function isCancelable(order: OrderResponse): boolean {
+    return order.status === OrderResponseStatusEnum.Created;
+}
+
+/**
+ * Returns true if the order can be returned.
+ * (For example, orders in the FINISHED state are considered returnable.)
+ */
+export function isReturnable(order: OrderResponse): boolean {
+    return order.status === OrderResponseStatusEnum.Finished;
+}
+
+/**
+ * Returns an object describing the available actions for the order.
+ */
+export function getOrderActions(order: OrderResponse) {
+    return {
+        canPay: isPayable(order),
+        canCancel: isCancelable(order),
+        canReturn: isReturnable(order),
+    };
 }
